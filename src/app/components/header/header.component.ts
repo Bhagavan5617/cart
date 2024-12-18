5436128713613674ce114c9d3f3978e606ef927d
bhagavan5617 <bhagavanpabbu23@gmail.com>
2024-12-18 09:48:21 +0530

    functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener, inject } from '@angular/core';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-header',
@@ -11,8 +12,9 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit{
+  selectedCategory:any ;
+  categories:any = []
   isFixed:boolean =false;
   @HostListener('window:scroll',[])
   onWindowScroll() {
@@ -20,7 +22,18 @@ export class HeaderComponent {
     this.isFixed = scrollPosition > 100; 
   }
   private modalService = inject(NgbModal);
-  
+  private productService = inject(ApiService)
+  ngOnInit(): void {
+    this.productService.getCategoriesObservable().subscribe(
+      categories => {
+        this.categories = categories;
+      },
+      error => {
+        console.error('Error fetching categories:', error);
+      }
+    );
+
+  }
   
     open() {
       this.modalService.open(LoginComponent).result.then(
@@ -28,5 +41,19 @@ export class HeaderComponent {
           
         },			
       );
+    }
+    onCategorySelect  (category: string): void {
+      this.selectedCategory = category;
+      if (category) {
+        this.productService.getProductsByCategory(category).subscribe(
+          products => {
+            console.log(category);
+            this.productService.updateProducts(products);
+          },
+          error => {
+            console.error('Error fetching products:', error);
+          }
+        );
+      }
     }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgbRatingConfig, NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +11,89 @@ import { NgbRatingConfig, NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
-  constructor(config: NgbRatingConfig) {
+  categories: any[] = [];
+  products: any[] = [];
+  selectedCategory: string = '';
 
-		config.max = 5;
-		config.readonly = true;
-	}
+
+  constructor(private productService: ApiService, config: NgbRatingConfig) {
+  
+    config.max = 5;
+    config.readonly = true;
+  }
+
+  ngOnInit(): void {
+    this.fetchCategories();
+    this.fetchAllProducts();
+
+    this.productService.getProductsObservable().subscribe(
+      products => {
+        this.products = products;
+      },
+      error => {
+        console.error('Error fetching products:', error);
+      }
+    );
+
+    // Subscribe to the shared observable for categories
+    this.productService.getCategoriesObservable().subscribe(
+      categories => {
+        this.categories = categories;
+      },
+      error => {
+        console.error('Error fetching categories:', error);
+      }
+    );
+
+
+
+
+  }
+
+  fetchCategories(): void {
+    this.productService.getCategories().subscribe(
+      data => {
+        this.categories = data;
+      },
+      error => {
+        console.error('Error fetching categories:', error);
+      }
+    );
+  }
+
+  fetchAllProducts(): void {
+    this.productService.getAllProducts().subscribe(
+      data => {
+        this.products = data;
+      },
+      error => {
+        console.error('Error fetching all products:', error);
+      }
+    );
+  }
+
+  onCategorySelect(name: string): void {
+    this.selectedCategory = name;
+    if (name) {
+      console.log(name)
+      this.fetchProductsByCategory(name);
+    } else {
+      this.fetchAllProducts();  
+    }
+  }
+
+  fetchProductsByCategory(category: string): void {
+    this.productService.getProductsByCategory(category).subscribe(
+      data => {
+        this.products = data;
+        console.log(this.products)
+      },
+      error => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
+
 }
